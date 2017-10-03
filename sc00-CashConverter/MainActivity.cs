341f6335f3b07da059e4562650eb7c937c18dd56
AdiMDCProfessor <azejnilo@mdc.edu>
2017-10-03 17:44:51 -0400

    Add project files.

---
+++ sc00-CashConverter/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace sc00_CashConverter
+{
+    [Activity(Label = "sc00_CashConverter", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
